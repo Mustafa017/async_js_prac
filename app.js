dc600c56ee353cfe0e204490684bc927bd1f3617
Mustafa017 <mustafangure95@gmail.com>
2020-08-18 15:47:35 -0500

    feat: Use fs Promise API

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 const express = require("express");
-const fs = require("fs");
+const fsPromises = require("fs").promises;// node version should be >= 10 
 const path = require("path");
-const { resolve } = require("path");
-const { rejects } = require("assert");
 
 const app = express();
 const port = process.env.PORT || 3000;
 const dataFilePath = path.resolve(__dirname, 'data/restaurants.json')
 
-const getJson = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(dataFilePath,'utf-8', (err, data)=>{
-            if(err){
-                reject(err); 
-            }else{
-                console.log('reading file data');
-                resolve(data);
-            }
-        });
-        console.log('after reading file');
-    });
+const getJson = (callback) => {
+    let jsonData = fsPromises.readFile(dataFilePath, 'utf-8')
+        .then(data => JSON.parse(data));
+    console.log(jsonData);
+    console.log('after reading file');
+    return(jsonData);
 }
 
 app.get('/data', (req, res)=>{
     getJson()
-        .then(data => {
-            let restaurantData = JSON.parse(data);
-            res.send(restaurantData);
-        })
+        .then(data => res.send(data));
 });
 
 app.get('/', (req, res)=>{
